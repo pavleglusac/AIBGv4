f1f4bb9619feae5ec48ced3224375f41e607bfa1
unknown <srdanov.ra145.2019@uns.ac.rs>
2023-11-05 23:39:26 +0100

    Pause menu

---
+++ Assets/GUI/Scripts/MenuNavigation.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuNavigation : MonoBehaviour
+{
+    public static bool IsPaused = false;
+    public GameObject pauseMenu;
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        IsPaused = true;
+    }
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        IsPaused = false;
+    }
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main menu");
+        IsPaused = false;
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1f;
+        IsPaused = false;
+    }
+    public void Quit()
+    {
+        IsPaused = false;
+        Application.Quit();
+    }
+
+
+}
--- Assets/GUI/Scripts/PulsatingText.cs
+++ Assets/GUI/Scripts/PulsatingText.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PulsatingLogo : MonoBehaviour
+public class PulsatingText : MonoBehaviour
 {
-    public Text text; // Reference to the Text component
-    readonly float pulsateDuration = 4.0f; // Duration for one complete pulsation (shrink and enlarge)
+    public Text text;
+    readonly float pulsateDuration = 4.0f;
     private float startTime;
 
     void Start()
@@ -18,12 +18,8 @@ public class PulsatingLogo : MonoBehaviour
     void Update()
     {
         float elapsedTime = Time.time - startTime;
-        float t = Mathf.Repeat(elapsedTime, pulsateDuration) / pulsateDuration; // Calculate a looping value within [0, 1]
-
-        // Calculate the scaling factor for pulsation (0.9 to 1.1)
+        float t = Mathf.Repeat(elapsedTime, pulsateDuration) / pulsateDuration;
         float scale = 0.9f + 0.2f * Mathf.Abs(0.5f - t) * 2;
-
-        // Apply the scale to the text component
         text.transform.localScale = new Vector3(scale, scale, 1.0f);
     }
 }
