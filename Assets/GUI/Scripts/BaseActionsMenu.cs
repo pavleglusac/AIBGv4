2f28d90dfaaae44d5ae207d664b775df344d245b
eXtremeNemanja <dutina.sv27.2020@uns.ac.rs>
2024-02-18 03:54:49 +0100

    feat: Added conversion rates

--- Assets/GUI/Scripts/BaseActionsMenu.cs
+++ Assets/GUI/Scripts/BaseActionsMenu.cs
@@ -12,6 +12,16 @@ public class BaseActionsMenu : MonoBehaviour
     private int EnergyCheap = 0;
     private int EnergyExpensive = 0;
 
+    private int XPCheapConversionRate = 0;
+    private int XPExpensiveConversionRate = 0;
+    private int CoinsCheapConversionRate = 0;
+    private int CoinsExpensiveConversionRate = 0;
+    private int EnergyCheapConversionRate = 0;
+    private int EnergyExpensiveConversionRate = 0;
+
+    private int CoinsTotal = 0;
+    private int XPTotal = 0;
+    private int EnergyTotal = 0;
 
     public static BaseActionsMenu Instance;
     public GameObject actionMenu;
@@ -30,6 +40,11 @@ public class BaseActionsMenu : MonoBehaviour
         Game.ResumeGame();
     }
 
+    public void UpdateText()
+    {
+        BaseActionsMenuGUI.Instance.UpdateText(Instance.XPCheap, Instance.XPExpensive, Instance.CoinsCheap, Instance.CoinsExpensive, Instance.EnergyCheap, Instance.EnergyExpensive, Instance.XPTotal, Instance.CoinsTotal);
+    }
+
     void Start()
     {
         // Debug.Log("Usao");
@@ -44,9 +59,10 @@ public class BaseActionsMenu : MonoBehaviour
         if ((Instance.XPCheap + Instance.CoinsCheap + Instance.EnergyCheap) < 5)
         {
             Instance.XPCheap++;
+            CalculateTotal();
         }
         Debug.Log(Instance.XPCheap);
-        // UpdateText();
+        UpdateText();
     }
 
     public void DecreaseXPCheapCrystal()
@@ -56,9 +72,10 @@ public class BaseActionsMenu : MonoBehaviour
         if (Instance.XPCheap > 0)
         {
             Instance.XPCheap--;
+            CalculateTotal();
         }
         Debug.Log(Instance.XPCheap);
-        // UpdateText();
+        UpdateText();
     }
 
     public void IncreaseXPExpensiveCrystal()
@@ -68,9 +85,10 @@ public class BaseActionsMenu : MonoBehaviour
         if ((Instance.XPExpensive + Instance.CoinsExpensive + Instance.EnergyExpensive) < 5)
         {
             Instance.XPExpensive++;
+            CalculateTotal();
         }
         Debug.Log(Instance.XPExpensive);
-        // UpdateText();
+        UpdateText();
     }
 
     public void DecreaseXPExpensiveCrystal()
@@ -80,9 +98,10 @@ public class BaseActionsMenu : MonoBehaviour
         if (Instance.XPExpensive > 0)
         {
             Instance.XPExpensive--;
+            CalculateTotal();
         }
         Debug.Log(Instance.XPExpensive);
-        // UpdateText();
+        UpdateText();
     }
 
 
@@ -93,9 +112,10 @@ public class BaseActionsMenu : MonoBehaviour
         if ((Instance.XPCheap + Instance.CoinsCheap + Instance.EnergyCheap) < 5)
         {
             Instance.CoinsCheap++;
+            CalculateTotal();
         }
         Debug.Log(Instance.CoinsCheap);
-        // UpdateText();
+        UpdateText();
     }
 
     public void DecreaseCoinsCheapCrystal()
@@ -105,9 +125,10 @@ public class BaseActionsMenu : MonoBehaviour
         if (Instance.CoinsCheap > 0)
         {
             Instance.CoinsCheap--;
+            CalculateTotal();
         }
         Debug.Log(Instance.CoinsCheap);
-        // UpdateText();
+        UpdateText();
     }
 
     public void IncreaseCoinsExpensiveCrystal()
@@ -117,9 +138,10 @@ public class BaseActionsMenu : MonoBehaviour
         if ((Instance.XPExpensive + Instance.CoinsExpensive + Instance.EnergyExpensive) < 5)
         {
             Instance.CoinsExpensive++;
+            CalculateTotal();
         }
         Debug.Log(Instance.CoinsExpensive);
-        // UpdateText();
+        UpdateText();
     }
 
     public void DecreaseCoinsExpensiveCrystal()
@@ -128,10 +150,11 @@ public class BaseActionsMenu : MonoBehaviour
         // if ((Instance.XPCheap + Instance.CoinsCheap + Instance.EnergyCheap) < player.Bag.GetCheapCrystalCount())
         if (Instance.CoinsExpensive > 0)
         {
-            Instance.XPExpensive--;
+            Instance.CoinsExpensive--;
+            CalculateTotal();
         }
         Debug.Log(Instance.XPExpensive);
-        // UpdateText();
+        UpdateText();
     }
 
 
@@ -142,9 +165,10 @@ public class BaseActionsMenu : MonoBehaviour
         if ((Instance.XPCheap + Instance.CoinsCheap + Instance.EnergyCheap) < 5)
         {
             Instance.EnergyCheap++;
+            CalculateTotal();
         }
         Debug.Log(Instance.EnergyCheap);
-        // UpdateText();
+        UpdateText();
     }
 
     public void DecreaseEnergyCheapCrystal()
@@ -154,9 +178,10 @@ public class BaseActionsMenu : MonoBehaviour
         if (Instance.EnergyCheap > 0)
         {
             Instance.EnergyCheap--;
+            CalculateTotal();
         }
         Debug.Log(Instance.EnergyCheap);
-        // UpdateText();
+        UpdateText();
     }
 
     public void IncreaseEnergyExpensiveCrystal()
@@ -166,9 +191,10 @@ public class BaseActionsMenu : MonoBehaviour
         if ((Instance.XPExpensive + Instance.CoinsExpensive + Instance.EnergyExpensive) < 5)
         {
             Instance.EnergyExpensive++;
+            CalculateTotal();
         }
         Debug.Log(Instance.EnergyExpensive);
-        // UpdateText();
+        UpdateText();
     }
 
     public void DecreaseEnergyExpensiveCrystal()
@@ -178,8 +204,23 @@ public class BaseActionsMenu : MonoBehaviour
         if (Instance.EnergyExpensive > 0)
         {
             Instance.EnergyExpensive--;
+            CalculateTotal();
         }
         Debug.Log(Instance.EnergyExpensive);
-        // UpdateText();
+        UpdateText();
+    }
+
+    public void CalculateTotal()
+    {
+        if (Instance.XPCheapConversionRate == 0) Instance.XPCheapConversionRate = int.Parse(PlayerPrefs.GetString("cheap_to_xp"));
+        if (Instance.XPExpensiveConversionRate == 0) Instance.XPExpensiveConversionRate = int.Parse(PlayerPrefs.GetString("exp_to_xp"));
+        if (Instance.CoinsCheapConversionRate == 0) Instance.CoinsCheapConversionRate = int.Parse(PlayerPrefs.GetString("cheap_to_coins"));
+        if (Instance.CoinsExpensiveConversionRate == 0) Instance.CoinsExpensiveConversionRate = int.Parse(PlayerPrefs.GetString("exp_to_coins"));
+        if (Instance.EnergyCheapConversionRate == 0) Instance.EnergyCheapConversionRate = int.Parse(PlayerPrefs.GetString("cheap_to_energy"));
+        if (Instance.EnergyExpensiveConversionRate == 0) Instance.EnergyExpensiveConversionRate = int.Parse(PlayerPrefs.GetString("exp_to_energy"));
+
+        Instance.XPTotal = Instance.XPCheap * Instance.XPCheapConversionRate + Instance.XPExpensive * Instance.XPExpensiveConversionRate;
+        Instance.CoinsTotal = Instance.CoinsCheap * Instance.CoinsCheapConversionRate + Instance.CoinsExpensive * Instance.CoinsExpensiveConversionRate;
+        Instance.EnergyTotal = Instance.EnergyCheap * Instance.EnergyCheapConversionRate + Instance.EnergyExpensive * Instance.EnergyExpensiveConversionRate;
     }
 }
