d8c40dc46c2c1640f427bf1c9312500cd778c604
eXtremeNemanja <dutina.sv27.2020@uns.ac.rs>
2024-02-18 22:16:42 +0100

    fix: movement bugfix

--- Assets/Scripts/Base.cs
+++ Assets/Scripts/Base.cs
@@ -19,16 +19,16 @@ public class Base : MonoBehaviour
 
     void Update()
     {
-        // if (Input.GetMouseButtonDown(1))
-        // {
-        //     Debug.Log(this.X + " " + this.Z);
-        //     Debug.Log("Name: " + gameObject.name);
-        //     if (this.X == 0 && this.Z == 0) return;
-        //     Debug.Log(this.IsFirstPlayers);
-        //     if (Game.Instance.GetCurrentPlayer().X != this.X || Game.Instance.GetCurrentPlayer().Z != this.Z) return;
-        //     // if (Game.Instance.GetCurrentPlayer().FirstPlayer != this.IsFirstPlayers) return;
-        //     BaseActionsMenu.Instance.OpenActionsMenu();
-        // }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(this.X + " " + this.Z);
+            Debug.Log("Name: " + gameObject.name);
+            if (this.X == 0 && this.Z == 0) return;
+            Debug.Log(this.IsFirstPlayers);
+            if (Game.Instance.GetCurrentPlayer().X != this.X || Game.Instance.GetCurrentPlayer().Z != this.Z) return;
+            // if (Game.Instance.GetCurrentPlayer().FirstPlayer != this.IsFirstPlayers) return;
+            BaseActionsMenu.Instance.OpenActionsMenu();
+        }
 
     }
 }
