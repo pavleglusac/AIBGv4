62b8b6dfab24f591c4eda3acb58fb6404c1383a2
Pavle Glusac <pavleglusac@gmail.com>
2024-03-09 23:02:33 +0100

    Add file browser plugin

--- Assets/GUI/Scripts/PlayerVsPlayerButtonClick.cs
+++ Assets/GUI/Scripts/PlayerVsPlayerButtonClick.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using SimpleFileBrowser;
 
 public class PlayerVsPlayerButtonClick : MonoBehaviour
 {
     public void StartGamePlayerVsPlayer()
     {
-        Game.Instance.ResetGame();
+        if (Game.Instance != null)
+        {
+            Game.Instance.ResetGame();
+        } 
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
