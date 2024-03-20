5f7bea6a55a608344bc40516c1a1b04ff6698c42
Jovan Srdanov <srdanov.ra145.2019@uns.ac.rs>
2024-03-20 23:29:21 +0100

    Fixed exiting game

--- Assets/Gascina.cs
+++ Assets/Gascina.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QuitGame : MonoBehaviour
+public class GameExiter : MonoBehaviour
 {
 
     public void ExitGame()
