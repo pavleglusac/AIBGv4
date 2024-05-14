efe94b5f6133d9bc556ac08128239db506c27f34
JovanSrdanov <jovansrdanov2000@gmail.com>
2024-05-14 20:13:11 +0200

    Set low and high resolution

--- Assets/GUI/Scripts/BackgroundMovement.cs
+++ Assets/GUI/Scripts/BackgroundMovement.cs
@@ -9,8 +9,41 @@ public class BackgroundMovement : MonoBehaviour
     private readonly float x = 0.05f;
     private readonly float y = 0.1f;
 
+    void Start()
+    {
+        SetHighGraphics();
+        Application.targetFrameRate = 60;
+    }
+
+
     void Update()
     {
         rawImage.uvRect = new Rect(rawImage.uvRect.position + new Vector2(x, y) * Time.deltaTime, rawImage.uvRect.size);
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            SetHighGraphics();
+            Application.targetFrameRate = 60;
+        }
+
+        // Check for low graphics setting key press (L)
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            SetLowGraphics();
+            Application.targetFrameRate = 30;
+        }
     }
+
+    void SetHighGraphics()
+    {
+        QualitySettings.SetQualityLevel(QualitySettings.names.Length - 1, true);
+        Debug.Log("High graphics settings applied.");
+    }
+
+    void SetLowGraphics()
+    {
+        QualitySettings.SetQualityLevel(0, true);
+        Debug.Log("Low graphics settings applied.");
+    }
+
 }
